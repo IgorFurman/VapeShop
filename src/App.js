b2376335f310f092173812caad334fb6896447d4
Igor <igorfurman555@gmail.com>
2023-04-27 13:02:22 +0200

    updated structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+ 
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context.js";
 
 function App() {
   return (
