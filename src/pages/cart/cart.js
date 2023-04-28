fe8cc7027d39a43ec2704ac17a4b7fa328caa814
Igor <igorfurman555@gmail.com>
2023-04-28 13:50:44 +0200

    added shopping card and styles changes

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,6 +1,30 @@
-import React from "react"
+import React, { useContext } from 'react';
+import { PRODUCTS } from '../../products';
+import { ShopContext } from '../../context/shop-context';
+import { CartItem } from './cart-item';
+import './cart.css';
 
 export const Cart = () => {
-    return
-}
-
+	const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+	const totalAmount = getTotalCartAmount();
+	return (
+		<div className='cart'>
+			<div>
+				<h2>Twój koszyk</h2>
+			</div>
+			<div className='cartItems'>
+				{PRODUCTS.map((product) => {
+					if (cartItems[product.id] !== 0) {
+						return <CartItem data={product} />;
+					} else {
+						return null;
+					}
+				})}
+			</div>
+			<div className='checkout'>
+				<p>Razem do zapłaty: {totalAmount}zł</p>
+				<button className=''>Przejdź do płatności</button>
+			</div>
+		</div>
+	);
+};
