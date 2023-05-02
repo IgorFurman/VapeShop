5a3cb1f02ba6f525741d2614cafbba668147ba1c
Igor <igorfurman555@gmail.com>
2023-05-02 14:50:49 +0200

    modal added and styles

--- src/pages/shop/product.js
+++ src/pages/shop/product.js
@@ -1,27 +1,75 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
 import { ShopContext } from '../../context/shop-context';
 
-export const Product = (props) => {
-  const { id, productName, price, productImage } = props.data;
-  const { addToCart, cartItems } = useContext(ShopContext);
-  const cartItemAmount = cartItems[id];
+import './product.css';
 
-  return (
-    <div className='product'>
-      <Link to={`/products/${id}`}>
-        <img src={productImage} alt={productName} />
-      </Link>
-      <div className='product-description'>
-        <p>
-          <b>{productName}</b>
-        </p>
-        <p>{price}zł</p>
-      </div>
-      <button className='addToCartBttn' onClick={() => addToCart(id)}>
-        Dodaj do koszyka {cartItemAmount > 0 && <>({cartItemAmount})</>}
-      </button>
-      {/* <Link className='product-details' to={`/products/${id}`}>Zobacz więcej</Link> */}
-    </div>
-  );
+export const Product = (props) => {
+	const { id, productName, price, productImage } = props.data;
+	const { addToCart, cartItems, removeFromCart, updateCartItemCount } =
+		useContext(ShopContext);
+	const cartItemAmount = cartItems[id];
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
+	const handleAddToCart = () => {
+		addToCart(id);
+		setIsModalVisible(true);
+	};
+
+	const handleCloseModal = () => {
+		setIsModalVisible(false);
+	};
+
+	return (
+		<>
+			<div className='product'>
+				<Link to={`/products/${id}`}>
+					<img src={productImage} alt={productName} />
+				</Link>
+				<div className='product-description'>
+					<p>
+						<b>{productName}</b>
+					</p>
+					<p>{price}zł</p>
+				</div>
+				<button className='addToCartBttn' onClick={handleAddToCart}>
+					Dodaj do koszyka {cartItemAmount > 0 && <>({cartItemAmount})</>}
+				</button>
+			</div>
+			<Modal
+				isOpen={isModalVisible}
+				onRequestClose={handleCloseModal}
+				className='popup-container'
+			>
+				<div className='popup-box'>
+					<h3 className='popup-message'>
+						{productName} został dodany do koszyka!
+					</h3>
+					<div className='popup-quantity-control'>
+						<img src={productImage} alt={productName} className='popup-image' />
+						<div className='popup-add-remove-container'>
+							<button onClick={() => removeFromCart(id)}> - </button>
+							<input
+								value={cartItems[id]}
+								onChange={(e) =>
+									updateCartItemCount(Number(e.target.value), id)
+								}
+								min='0'
+							/>
+							<button onClick={() => addToCart(id)}> + </button>
+						</div>
+					</div>
+					<div className='popup-buttons'>
+						<button onClick={handleCloseModal} className='popup-button'>
+							Kontynuuj zakupy
+						</button>
+						<Link to='/cart' className='popup-button'>
+							Przejdź do koszyka
+						</Link>
+					</div>
+				</div>
+			</Modal>
+		</>
+	);
 };
